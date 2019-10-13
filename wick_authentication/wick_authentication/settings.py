d2e28338f6daa55b22d09b270b6f06ad98201af9
nvaitoonkiat <nvaitoonkiat@gmail.com>
2019-10-13 18:58:02 -0400

    Switched over to Mysql instead of SQLite

--- wick_authentication/wick_authentication/settings.py
+++ wick_authentication/wick_authentication/settings.py
@@ -68,8 +68,14 @@ WSGI_APPLICATION = 'wick_authentication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wick_login',
+        'USER' : 'root',
+        'PASSWORD' : '#########',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
